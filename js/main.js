d6b0b685b8457d8ca5c47f3a66cb6349e4984b56
Christian Arevalo <carevaloq87@gmail.com>
2016-11-15 14:03:50 +1100

    -Header bar -Date selector -Menu Selector -Event after click Date or menu -Ajax POST call with date and menu parameters -Mustache JS LIbrary support to render html templates from JSON objects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-var currentDate = new Date();
 
+var selected_date = get_current_date();
+var selected_menu = "";
 $(function() {
+	selected_menu = $("#calendar_list li")[0].id;
 	init_calendar();
 	click_calendars();
+	hide_loader();
 });
 
 function get_current_date() {
+	var currentDate = new Date();
 	var year  = currentDate.getFullYear();
 	var month = currentDate.getMonth() + 1;
 	var day   = currentDate.getDate() ;
@@ -19,8 +23,11 @@ function init_calendar() {
 	    //daysOfWeekHighlighted: "1,2,3,4,5",
 		format: "yyyy-mm-dd",
 		todayHighlight: true
-	}).on("changeDate", function(e) {
-        console.log(e.format(0,"yyyy-mm-dd"));       
+	}).on("changeDate", function(e) {		        
+        selected_date = e.format(0,"yyyy-mm-dd");   
+        console.log(selected_date);
+        console.log(selected_menu); 
+		get_events();
     });
 }
 
@@ -39,5 +46,27 @@ function click_calendars(){
 	    $("#current-calendar").text($(this).text());
 	    $('#calendar_list li.active').removeClass('active');
 	    $(this).addClass('active');
+		selected_menu = $(this).attr("id");
+		get_events();
 	});
+}
+
+function get_events(){
+	show_loader();  	
+  	var data_url = "example-data.txt";
+  	$.post( data_url, { date: selected_date, menu_id: selected_menu })
+	  .done(function( data ) {
+	    var cards = JSON.parse(data);
+    	render_cards(cards);
+	  });
+	hide_loader();
+}
+
+function render_cards(cards) {
+	var targetContainer = $(".events-container"),
+    template = $("#messages-template").html();
+
+	var html = Mustache.to_html(template, cards);
+
+	$(targetContainer).html(html);
 }
